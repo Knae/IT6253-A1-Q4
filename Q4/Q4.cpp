9522540d5b9ae202f00abf18689b16bcf8535a57
Chuang Sing, Kee <general124@hotmail.com>
2014-09-06 23:59:00 +1200

    Working deep copy implementation

--- Q4/Q4.cpp
+++ Q4/Q4.cpp
@@ -16,6 +16,7 @@ public:
 	~Wheel() { delete ptrSize; }
 
 	void pump(int amount) { pressure += amount; }
+	int getPressure() { return pressure; }
 
 private:
 	int *ptrSize;
@@ -25,18 +26,27 @@ private:
 class RacingCar
 {
 public:
-	RacingCar() { RacingCar(0); }
+	RacingCar();
 	RacingCar(int);
+	RacingCar(const RacingCar &oldobject);
+	Wheel *getWheel(int wheel) { return carWheels[wheel]; }
+	void printCar();
 	~RacingCar();
-	RacingCar(const RacingCar &oldobject)
-	{
-
-	}
 
 private:
 	int speed;
+	RacingCar *carbonCopy;
 	Wheel* carWheels[4];
 };
+
+RacingCar::RacingCar()
+{
+	speed = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		carWheels[i] = new Wheel();
+	}
+}
 
 RacingCar::RacingCar(int s)
 {
@@ -45,7 +55,25 @@ RacingCar::RacingCar(int s)
 	{
 		carWheels[i] = new Wheel();
 	}
+}
 
+RacingCar::RacingCar(const RacingCar &oldobject)
+{
+	speed = oldobject.speed;
+	for (int i = 0; i < 4; i++)
+	{
+		carWheels[i] = new Wheel(32, oldobject.carWheels[i] ->getPressure() );
+	}
+}
+
+void RacingCar::printCar()
+{
+	cout << "Wheel pressure: " << endl;
+	for (int i = 0; i < 4; i++)
+	{
+		printf("Wheel no.%i = %i \n", i+1, carWheels[i] ->getPressure() );
+	}
+	cout << endl;
 }
 
 RacingCar :: ~RacingCar()
@@ -55,11 +83,28 @@ RacingCar :: ~RacingCar()
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	RacingCar rc;
-	RacingCar rc2( 5 );
+	RacingCar *rc = new RacingCar;
+	RacingCar *rc2 = new RacingCar( 5 );
 	RacingCar *ptrRcar = new RacingCar(6);
 
+	cout << "Racing Car - rc" << endl;
+	rc->printCar();
+
+	RacingCar rcCopy = *rc;
+	cout << "Racing Car - rcCopy" << endl;
+	rcCopy.printCar();
+
+	//pump air into 3 wheel of car rc
+	rc -> getWheel(2) ->pump(20);
+	rc -> getWheel(3) ->pump(-20);
+	
+	cout << "Racing Car - rc" << endl;
+	rc->printCar();
+	cout << "Racing Car - rcCopy" << endl;
+	rcCopy.printCar();
+
 	delete ptrRcar;
+	_getch();
 	return 0;
 }
 
